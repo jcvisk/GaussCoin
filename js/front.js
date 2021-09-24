10a581d7fa01bb47b5c9d6e6c514fc093ff1f5c3
jcvisk <juan.carlos.gcn@gmail.com>
2021-09-24 12:22:26 -0500

    corrgiendo circuito

--- js/front.js
+++ js/front.js
@@ -1,10 +1,22 @@
 $(window).scroll(function(){
     if ($("#nav").offset().top > 56) {
         /*Cambiando color de navbar*/
+        $("#nav").removeClass("bg-transparent");
+        $("#nav").addClass("glass");
+        /* Removiendo padding */
         $("#nav").removeClass("_p-5");
+        /*Cambiado tamaño de logo*/
+        $("#logo").removeClass("img-rps");
+        $("#logo").addClass("_img-rps");
     }else{
         /*Cambiando color de navbar*/
+        $("#nav").removeClass("glass");
+        $("#nav").addClass("bg-transparent");
+        /* Añadiendo padding */
         $("#nav").addClass("_p-5");
+        /*Cambiado tamaño de logo*/
+        $("#logo").addClass("img-rps");
+        $("#logo").removeClass("_img-rps");
     }
 });
 
